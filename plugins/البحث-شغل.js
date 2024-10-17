2962e3c58460b2b238311a31bd150e00f7fe3dc0
Maestro-22 <uvivucuf4@gmail.com>
2024-10-18 02:47:14 +0300

    Update البحث-شغل.js

--- plugins/البحث-شغل.js
+++ plugins/البحث-شغل.js
@@ -1,46 +1,109 @@
+/*
+╮────────────────────────╭ـ
+│ By : 𝗦𝗔𝗬𝗘𝗗-𝗦𝗛𝗔𝗪𝗔𝗭𝗔 🧞
+│ Number : https://wa.me/201145624848
+│ Community : https://chat.whatsapp.com/Hg4F5jQ9Z9r1lUH6I1jkhI
+│ Group Support : https://chat.whatsapp.com/JGtNRFwfHJC8XholdKmVGS
+│ Chanel : https://whatsapp.com/channel/0029Vael6wMJP20ze3IXJk0z
+╯────────────────────────╰ـ 
+*/
+
+
+
+import fetch from 'node-fetch';
 import yts from 'yt-search';
+import ytdl from 'ytdl-core';
+import axios from 'axios';
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
+import { prepareWAMessageMedia, generateWAMessageFromContent } from '@whiskeysockets/baileys';
 
-let handler = async (m, { conn, command, args, text, usedPrefix }) => {
+const handler = async (m, { command, usedPrefix, conn, args, text }) => {
+
     if (!text) {
-        return conn.reply(m.chat, '- *🔱 اكتب اسم الفيديو او الاغنية الذي تريد البحث عنها في يوتيوب وتشغيلها*', m);
+      await conn.sendMessage(m.chat, { text: `*❲ ❗ ❳ يرجي إدخال نص للبحث في يوتيوب .*\nمثال :\n> ➤  ${usedPrefix + command} القرآن الكريم\n> ➤  ${usedPrefix + command} https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p` }, { quoted: m });
+      await conn.sendMessage(m.chat, { react: { text: '❗', key: m.key } });
+      return;
+    }
+    
+    await conn.sendMessage(m.chat, { react: { text: '⏳', key: m.key } });
+    
+    try {
+      const yt_play = await search(text);
+      const dataMessage = `*❲  🧞نتيجة البحث عن : ${text} ❳*\n\n➤ العنوان : ${yt_play[0].title}\n➤ النشر : ${yt_play[0].ago}\n➤ الطول : ${secondString(yt_play[0].duration.seconds)}\n➤ الرابط : ${yt_play[0].url}\n➤ المشاهدات : ${MilesNumber(yt_play[0].views)}\n➤ الصانع : ${yt_play[0].author.name}\n➤ القناة : ${yt_play[0].author.url}`.trim();
+
+      const iturl = yt_play[0].url;
+      const itimg = yt_play[0].thumbnail;
+      const messa = await prepareWAMessageMedia({ image: { url: itimg } }, { upload: conn.waUploadToServer });
+
+      let msg = generateWAMessageFromContent(m.chat, {
+        viewOnceMessage: {
+          message: {
+            interactiveMessage: {
+              body: { text: dataMessage },
+              footer: { text: `© ${global.wm}`.trim() },
+              header: {
+                hasMediaAttachment: true,
+                imageMessage: messa.imageMessage,
+              },
+              nativeFlowMessage: {
+                buttons: [
+                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: '〘 🎧 صــوتي 〙', id: `${usedPrefix}اغنية ${iturl}` }) },
+                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: '〘 🎥 فيــديو 〙', id: `${usedPrefix} فيديو ${iturl}` }) },
+                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: '〘 🎤 فــويس 〙', id: `${usedPrefix}ريك ${iturl}` }) },
+                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: '〘 📹 جيــف 〙', id: `${usedPrefix}جيف ${iturl}` }) },
+                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: '〘 📻 ملــف صــوتي 〙', id: `${usedPrefix}صوت ${iturl}` }) },
+                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: '〘 📺 ملــف فيــديو 〙', id: `${usedPrefix}فيد ${iturl}` }) }
+                ],
+                messageParamsJson: "",
+              },
+            },
+          },
+        },
+      }, { userJid: conn.user.jid, quoted: m });
+
+      await conn.sendMessage(m.chat, { react: { text: '✔️', key: m.key } });
+      
+      await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
+
+    } catch {
+      await conn.sendMessage(m.chat, { text: `*❲ ❗ ❳ حدث خطأ عند البحث في يوتيوب .*\nيرجي ادخال نص صحيح أو رابط مثال :\n> ➤  ${usedPrefix + command} القرآن الكريم\n> ➤  ${usedPrefix + command} https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p` }, { quoted: m });
+      await conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
     }
 
-    let res = await yts(text);
-    let play = res.videos[0];
-
-    if (!play) {
-        throw `- *🔱 حــدث خــطــأ*`;
-    }
-
-    let { title, thumbnail, ago, timestamp, views, videoId, url } = play;
-
-    let txt = '*✧━═══━⊰🎶⊱━═══━✧*\n';
-    txt += `*❐⤶ العنوان ↜* _${title}_\n\n`;
-    txt += `*❐⤶ رابط المقطع ↜* _https://www.youtube.com/watch?v=${videoId}_\n`;
-    txt += '*✠ ── ✷ ─ ‹🕷️› ─ ✷ ── ✠*\n\n';
-    txt += `*❐⤶ تم النشر منذ ↜* _${ago}_\n`;
-    txt += `*❐⤶ مدة الفيديو ↜* _${timestamp}_\n`;
-    txt += `*❐⤶ عدد المشاهدات ↜* _${views.toLocaleString()}_\n`;
-    txt += `*✠ ── ✷ ─ ‹🕷️› ─ ✷ ── ✠*`;
-
-    await conn.sendButton(m.chat, 
-        txt, 
-        '*ʙy:ᴛᴀɴᴊɪʀᴏ𖣬ʙᴏᴛ*', 
-        thumbnail, 
-        [
-            ['🎧 تشغيل كصوت', `${usedPrefix}اغنية ${url}`],
-            ['📄 تشغيل كملف صوتي', `${usedPrefix}شغل_كصوت ${text}`],
-            ['🎥 تشغيل كفيديو', `${usedPrefix}شغل_كفيديو ${url}`],
-            ['🎥 تشغيل ملف كفيديو', `${usedPrefix}شغل_كفيديو2 ${url}`]
-        ], 
-        m
-    );
-
-    // يمكنك إضافة أي رسالة هنا إذا كنت بحاجة
 };
 
-handler.help = ['play', 'play2', 'ytmp3'];
-handler.tags = ['dl'];
-handler.command = ['شغل'];
-
+handler.command = /^(شغل)$/i;
 export default handler;
+
+async function search(query, options = {}) {
+  const search = await yts.search({ query, hl: 'ar', gl: 'AR', ...options });
+  return search.videos;
+}
+
+function MilesNumber(number) {
+  const exp = /(\d)(?=(\d{3})+(?!\d))/g;
+  const rep = '$1.';
+  const arr = number.toString().split('.');
+  arr[0] = arr[0].replace(exp, rep);
+  return arr[1] ? arr.join('.') : arr[0];
+}
+
+function secondString(seconds) {
+  seconds = Number(seconds);
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  const dDisplay = d > 0 ? d + (d == 1 ? ' día, ' : ' días, ') : '';
+  const hDisplay = h > 0 ? h + (h == 1 ? ' hora, ' : ' horas, ') : '';
+  const mDisplay = m > 0 ? m + (m == 1 ? ' minuto, ' : ' minutos, ') : '';
+  const sDisplay = s > 0 ? s + (s == 1 ? ' segundo' : ' segundos') : '';
+  return dDisplay + hDisplay + mDisplay + sDisplay;
+}
+
+function bytesToSize(bytes) {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  if (bytes === 0) return 'n/a';
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+  return i === 0 ? `${bytes} ${sizes[i]}` : `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`;
+                     }
